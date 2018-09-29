17c31779590d6e90cb3a7d959f91d5e332cc03c6
egrodo <egrodo1@gmail.com>
2018-09-28 20:14:00 -0400

    Full responsiveness with resize listener, debounce, better media queries

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,10 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
         "implicit-arrow-linebreak": 0,
+        "react/destructuring-assignment": 0,
+    },
+    "env": {
+        "browser": true,
+        "node": true,
     },
 };
--- src/components/ImgPresentation.jsx
+++ src/components/ImgPresentation.jsx
@@ -15,20 +15,43 @@ class ImgPresentation extends PureComponent {
       small: false,
     };
 
-    this.resize = this.resize.bind(this);
+    this.resize = this.debounce(200, this.resize.bind(this));
   }
 
   componentDidMount() {
-    document.addEventListener('resize', this.resize);
+    if (this.state.dir === 'left') {
+      if (window.innerWidth < 1000) this.setState({ small: true });
+      window.addEventListener('resize', this.resize);
+    }
   }
 
+
   componentWillReceiveProps(nextProps) {
-    // If the data is updated, update it.
+    // Update state if there's new props.
     this.setState(nextProps);
   }
 
-  resize(test) {
-    console.log(test);
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize);
+  }
+
+  resize() {
+    // BUG: the direction also controls which one is on top when small screen size.
+    // In order to fix, implement a resize handler with a debounce to disable dir if width < 1000px.
+    if (window.innerWidth < 1000) {
+      this.setState({ small: true });
+    } else if (this.state.small) this.setState({ small: false });
+  }
+
+  debounce(delay, fn) {
+    let timerId;
+    return function () {
+      if (timerId) clearTimeout(timerId);
+      timerId = setTimeout(() => {
+        fn();
+        timerId = null;
+      }, delay);
+    }
   }
 
   render() {
@@ -38,27 +61,24 @@ class ImgPresentation extends PureComponent {
 
     // Ternary rendering to handle dir changes. Could also use conditional rendering
     // or switch statement, but ternary is faster iirc.
-
-    // BUG: the direction also controls which one is on top when small screen size.
-    // In order to fix, implement a resize handler with a debounce to disable dir if width < 1000px.
     return (
-      <div className="ImgPresentation">
-        {small || dir === 'left' ? (
+      <div className={`ImgPresentation${dir === 'right' ? ' right' : ' left'}`}>
+        {small || dir === 'right' ? (
           <Fragment>
-            <img src={image} alt={title} className="img" />
             <div className="textContent">
               <h1 className="title">{title}</h1>
               <p className="content">{content}</p>
             </div>
+            <img src={image} alt={title} className="img" />
           </Fragment>
         )
           : (
             <Fragment>
+              <img src={image} alt={title} className="img" />
               <div className="textContent">
                 <h1 className="title">{title}</h1>
                 <p className="content">{content}</p>
               </div>
-              <img src={image} alt={title} className="img" />
             </Fragment>
           )}
       </div>
